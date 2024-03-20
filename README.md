d24ad6373f495740282e96621411d6e9aab399f5
rorense <rkorense@gmail.com>
2024-03-21 11:28:55 +1300

    Document editing area: title, icon, cover

--- README.md
+++ README.md
@@ -43,3 +43,4 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/deploym
 - Clerk (authentication)
 - Sonner (toast)
 - Zustand
+- Emoji Picker React (emoji)
--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,38 @@
-const DocumentIdPage = () => {
-  return <div>Document</div>;
+"use client";
+
+import Toolbar from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+
+// * Same effect can be achieved using useParams Hooks
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return <p>Loading..</p>;
+  }
+
+  if (document === null) {
+    return <p>Document not found</p>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
