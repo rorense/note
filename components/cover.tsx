1df7ecf6efb8a650fb94ceab4a9b4085791daa99
rorense <rkorense@gmail.com>
2024-03-25 21:28:58 +1300

    delete and replace cover photo on edgestore

--- components/cover.tsx
+++ components/cover.tsx
@@ -9,6 +9,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverProps {
   url?: string;
@@ -19,8 +20,12 @@ const Cover = ({ url, preview }: CoverProps) => {
   const coverImage = useCoverImage();
   const removeCoverImage = useMutation(api.documents.removeCoverImage);
   const params = useParams();
+  const { edgestore } = useEdgeStore();
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({ url: url });
+    }
     removeCoverImage({ id: params.documentId as Id<"documents"> });
   };
 
@@ -36,7 +41,7 @@ const Cover = ({ url, preview }: CoverProps) => {
       {url && !preview && (
         <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className="text-muted-foreground text-xs"
             variant="outline"
             size="sm"
